be10027cc8863c2a27486b1f533172e657217060
Pure <zhubaiting_19@qq.com>
2026-05-10 20:16:41 +0800

    Created project and initialized it

---
+++ M3/xmake.lua
@@ -0,0 +1,4 @@
+target("M3")
+    set_kind("static")
+    add_files("**.cppm", {public = true})
+    -- add_files("**.cpp")
---
+++ xmake.lua
@@ -0,0 +1,15 @@
+-- using xmake version 3.08 or later
+
+set_arch("x64")
+add_rules("mode.debug", "mode.release")
+
+set_toolchains("gcc")
+add_links("stdc++exp")
+
+set_languages("c++26")
+set_policy("build.c++.modules", true)
+set_policy("build.c++.modules.std", true)
+set_policy("build.c++.modules.reuse", true)
+
+includes "test"
+includes "M3"
